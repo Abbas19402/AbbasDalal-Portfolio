a7f803b2a82acef153d174f77d426442b4e127d0
Abbas Dalal <abbas.dalal19402@gmail.com>
2023-11-19 18:54:23 +0530

    Completed UI (NF) without functionality 2

--- PageComponents/Landing/Connect/index.tsx
+++ PageComponents/Landing/Connect/index.tsx
@@ -16,17 +16,17 @@ const Connect: NextPage<ScriptProps> = () => {
                     <span className="text-5xl text-white tracking-wider">Get a quote</span>
                     <span className="text-xl text-gray-400">Fill up the form and I will get back to you<br className="hidden lg:block"/> within 24 hours.</span>
                 </div>
-                <div className="flex flex-col mx-20 gap-y-4 my-10">
-                    <div className="w-80 h-16 rounded-lg  border-2 bg-white/10 hover:bg-transparent border-transparent hover:border-white flex flex-row justify-between items-center px-5 hover:cursor-pointer">
+                <div className="flex flex-col gap-y-4 my-10 w-full px-5 lg:px-20">
+                    <div className="w-full lg:w-80 h-16 rounded-lg  border-2 bg-white/10 hover:bg-transparent border-transparent hover:border-white flex flex-row justify-between items-center px-5 hover:cursor-pointer">
                         <Image src={Mail} alt="phone" width={30} height={30}/>
                         <span className="tracking-wider text-white text-md">abbasali.dalal07@gmail.com</span>
                     </div>
-                    <div className="w-80 h-16 rounded-lg border-2 bg-white/10 hover:bg-transparent border-transparent hover:border-white flex flex-row justify-between items-center px-5 hover:cursor-pointer">
+                    <div className="w-full lg:w-80 h-16 rounded-lg border-2 bg-white/10 hover:bg-transparent border-transparent hover:border-white flex flex-row justify-between items-center px-5 hover:cursor-pointer">
                         <Image src={Location} alt="phone" width={30} height={30}/>
                         <span className="tracking-wider text-white text-md">Rajasthan - India</span>
                     </div>
                 </div>
-                <div className="my-10 mx-20 flex justify-between items-center w-80 gap-x-4">
+                <div className="my-10 lg:mx-20 mx-0 flex justify-between items-center w-full px-5 lg:px-0 lg:w-80 gap-x-4">
                     <div className="rounded-2xl h-14 w-14 flex flex-row justify-center items-center relative border-2 bg-white/10 hover:bg-transparent border-transparent hover:border-white">
                         <Image src={Instagram} alt="phone" width={30} height={30}/>
                     </div>
@@ -41,7 +41,7 @@ const Connect: NextPage<ScriptProps> = () => {
             <div className="w-full min-h-screen h-full flex flex-col justify-center items-center">
                 <div className="flex flex-col my-14 mx-20 gap-y-5 lg:hidden w-full px-6">
                     <span className="text-5xl text-white tracking-wider">Get a quote</span>
-                    <span className="text-xl text-gray-400">Fill up the form and I will get back to you<br className="hidden lg:block"/> within 24 hours.</span>
+                    <span className="text-xl text-gray-400">Fill up the form and I will revert to you<br className="hidden lg:block"/> within 24 hours.</span>
                 </div>
                 <div className="w-full">
                     <Forms.ContactForm />
