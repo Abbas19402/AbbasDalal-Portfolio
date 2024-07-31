7eaec25836c006cbbc96bc4f2e6033bd9a23c24b
AAD-Zephyr <aad.zephyr.team@gmail.com>
2024-07-31 16:32:45 +0530

    bug fix: Project Card UI

--- Components/Cards/Projects/index.tsx
+++ Components/Cards/Projects/index.tsx
@@ -13,13 +13,14 @@ interface Props {
 }
 
 const ProjectCard: React.FC<Props> = ({ gradient, description , shortDesc , techUsed ,title , github, imageSrc }) => {
+    console.log(gradient)
     return(
         <div className="flex flex-col justify-between items-center">
             <div className="rounded-[16px] bg-black flex flex-col lg:flex-row gap-10 justify-between items-start mx-auto transition-all duration-700 w-full h-[80%]">
                     <div className="w-full flex flex-col lg:flex-row justify-between items-center h-full py-2">
                         <div className="h-full flex flex-col justify-between items-center ">
                             <div className="flex md:flex-row flex-col justify-between lg:items-center w-full">
-                                <div className={`bg-gradient-to-r from-${gradient.From} via-${gradient.Via} to--${gradient.To} bg-clip-text mb-2`}>
+                                <div className={`bg-gradient-to-r from-${gradient.From} via-${gradient.Via} to-${gradient.To} bg-clip-text mb-2`}>
                                     <span className="text-4xl text-transparent font-extrabold">{title}</span>
                                 </div>
                                 /
