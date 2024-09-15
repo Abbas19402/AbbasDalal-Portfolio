76751c3c46db6e5632d3387734de5f9c522f5c90
Abbas Ali Dalal <abbas.dalal19402@gmail.com>
2024-09-15 17:06:51 +0530

    Update fix: Cursor

--- Components/Cards/Skills/SkillCard.tsx
+++ Components/Cards/Skills/SkillCard.tsx
@@ -10,7 +10,7 @@ const SkillCard:React.FC<Props> = ({ skillName, iconSrc }) => {
         <div
             className="min-w-[70vw] md:min-w-[20vw] lg:min-w-[15vw] w-full h-12 flex flex-row justify-between items-center px-5 gap-x-4 rounded-lg hover:bg-white transition-all duration-500 hover:scale-110 group"
             >
-            <div className="text-md font-light tracking-wider text-white group-hover:text-black transition-all duration-500">
+            <div className="cursor-default text-md font-light tracking-wider text-white group-hover:text-black transition-all duration-500">
                 {skillName}
             </div>
             <div className="h-8 w-8 flex items-center justify-center relative rounded-lg overflow-hidden">
